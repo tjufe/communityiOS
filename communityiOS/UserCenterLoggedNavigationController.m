d9029aa1790d82b20fc235a4a53c3787ee4dc7a0
sdoxinxin1990 <sdoxinxin1990@qq.com>
2015-04-01 13:04:13 +0800

    up

---
+++ communityiOS/UserCenterLoggedNavigationController.m
@@ -0,0 +1,37 @@
+//
+//  UserCenterLoggedNavigationController.m
+//  communityiOS
+//
+//  Created by 何茂馨 on 15/4/1.
+//  Copyright (c) 2015年 &#20309;&#33538;&#39336;. All rights reserved.
+//
+
+#import "UserCenterLoggedNavigationController.h"
+
+@interface UserCenterLoggedNavigationController ()
+
+@end
+
+@implementation UserCenterLoggedNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
