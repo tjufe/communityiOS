72317a71a559eea8e1ea018660c4ee15617ab331
sdoxinxin1990 <sdoxinxin1990@qq.com>
2015-04-01 13:55:43 +0800

    hmx201504011355

---
+++ communityiOS/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  communityiOS
+//
+//  Created by 何茂馨 on 15/4/1.
+//  Copyright (c) 2015年 &#20309;&#33538;&#39336;. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
