28a4b2007b71d3c744dd30c48650abe6c4afcd20
sunxiaoyuan <sun3188360@163.com>
2015-04-08 15:51:06 +0800

    post

--- communityiOS/StatusTool.h
+++ communityiOS/StatusTool.h
@@ -10,12 +10,21 @@
 
 
 //请求板块列表
-typedef void(^ForumListSuccess)(NSArray * array);
+typedef void(^ForumListSuccess)(id object);
 typedef void(^ForumListFailurs)(NSError * error);
 
 @interface StatusTool : NSObject
 
-//请求板块列表
+//请求加载板块列表
 + (void)statusToolGetForumListWithID:(NSString *)ID  Success:(ForumListSuccess)success failurs:(ForumListFailurs)failure;
+
+//发送用户登录请求
++(void)statusToolGetUserLoginWithName:(NSString *)Name  PassWord:(NSString *)Pwd Success:(ForumListSuccess)success failurs:(ForumListFailurs)failure;
+
+//发送注册请求
++(void)statusToolGetUserRegWithName:(NSString *)Name  PassWord:(NSString *)Pwd Phone:(NSString *)Phone RegID:(NSString *)RegID Success:(ForumListSuccess)success failurs:(ForumListFailurs)failure;
+
+//请求加载帖子列表
++(void)statusToolGetPostListWithbfID:(NSString *)bfID  bcID:(NSString *)bcID userID:(NSString *)userID filter:(NSString *)filter Success:(ForumListSuccess)success failurs:(ForumListFailurs)failure;
 
 @end
